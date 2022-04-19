5a68a00fcdf632962cb2557b5ac8751b95111971
Namit Malasi <namitmalasi@gmail.com>
2022-04-19 16:48:14 +0530

    initialised a new next.js app

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Pizza Eatery</title>
+        <meta name="description" content="Best pizza shop in the city" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      homepage
+    </div>
+  );
+}
