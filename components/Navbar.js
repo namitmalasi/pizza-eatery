7379d3455f009e43af444b072b8c18bdf354d46d
Namit Malasi <namitmalasi@gmail.com>
2022-04-20 15:17:41 +0530

    created navbar

---
+++ components/Navbar.js
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import React from "react";
+import styles from "../styles/Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <Image src="/img/telephone.png" alt="" width="32" height="32" />
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>Order Now!</div>
+          <div className={styles.text}>123 456 789</div>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Homepage</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <h1 className={styles.logo}>Pizza Eatery</h1>
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <Image src="/img/cart.png" alt="" width="30px" height="30px" />
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
